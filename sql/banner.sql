209d9e786ac95b600136e0096026489e464a25ea
AvariciaTZ <94759980+AvariciaTZ@users.noreply.github.com>
2021-11-20 13:51:48 -0500

    Add files via upload

---
+++ sql/banner.sql
@@ -0,0 +1,9 @@
+create database Banner
+create table banner(
+id int primary key,
+bnr1 varchar(100),
+bnr2 varchar(100),
+bnr3 varchar(100)
+)
+insert into banner(id,bnr1,bnr2,bnr3) values (1,'img/img__banner/2.jpg','img/img__banner/0.jpg','img/img__banner/1.jpg');
+select*from banner
